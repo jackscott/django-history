87e3663257e41e701669e1506cf63df2e2b36e0b
Jack Scott <jscott@modernconsumer.com>
2009-07-02 02:47:30 -0400

    initial import

---
+++ admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.modelhistory.models import ChangeLog
+
+class ChangeLogAdmin(admin.ModelAdmin):
+    date_hierarchy = 'change_time'
+    list_filter = ['change_time',  'change_type', 'content_type']
+    #
+    #fieldsets = (
+    #    ('Meta info', {'fields': ('change_time', 'content_type', 'object_id', 'user', 'revision', 'revert_from')} ),
+    #    ('Object', {'fields': ('object',)}),
+    #    )
+
+    list_display = ('__str__', 'user', 'change_type', 'content_type', 'change_time', 'revision', 'revert_from')
+
+admin.site.register(ChangeLog, ChangeLogAdmin )
